d150322242be03aec075277e26b16a86f04163f7
jaynish23 <138020205+jaynish23@users.noreply.github.com>
2025-02-24 18:53:07 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
-import './App.css';
-import EventDemo from './EventDemo';
 function App() {
   return (
-   <>
-   <EventDemo />
-   </>
+    <>
+      <h1>Welcome to Website</h1>
+      <Router>
+        <nav>
+          <Link to="/home">Home | </Link>
+          <Link to="/about">About</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
+// import React from 'react'
+// import Demo from './Demo';
+// import DemoClass from './DemoClass';
+// import './App.css'
+// function App() {
+//   var a=10;
+//   var b= 20;
+//   let myColor = {
+//     backgroundColor : 'blue',
+//     color : 'white'
+//   }
+
+//   return (
+//     <>
+//       <Demo />
+//       <DemoClass />
+//     <h1 style={myColor}> App </h1>
+//     <h1 style={{backgroundColor:'red', color:'white'}}> Application inline css </h1>
+//     <h2 className='myClass'>external csss</h2>
+//     <br />
+//     <div>
+    
+//     value of a is {a}
+//     value of b is {b}
+//     {a+b}
+//     </div>
+//   </>
+//   )
+// }
+
+// export default App
+
+
